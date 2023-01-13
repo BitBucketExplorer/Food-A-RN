7d88eb5e29e2b0d37dd33d751fff31d871a2aa79
codeHub <vy.code92@gmail.com>
2023-01-14 04:04:31 +0530

    trying drawer errer fix

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -4,6 +4,7 @@ import SignInWelcome from '../screens/authScreens/SignInWelcome';
 import SignIn from '../screens/authScreens/SignIn';
 import Home from '../screens/Home';
 import RootClientTabs from './ClientTabs';
+import RestaurantsMap from '../screens/RestaurantsMap';
 import DrawerNavigatorTab from './DrawerNavigatorTab';
 const Auth = createStackNavigator();
 
@@ -22,6 +23,22 @@ const AuthNavigator = () => {
       <Auth.Screen
         name="SignIn"
         component={SignIn}
+        options={{
+          headerShown: false,
+          ...TransitionPresets.RevealFromBottomAndroid,
+        }}
+      />
+      <Auth.Screen
+        name="RootClientTabs"
+        component={RootClientTabs}
+        options={{
+          headerShown: false,
+          ...TransitionPresets.RevealFromBottomAndroid,
+        }}
+      />
+      <Auth.Screen
+        name="RestaurantsMap"
+        component={RestaurantsMap}
         options={{
           headerShown: false,
           ...TransitionPresets.RevealFromBottomAndroid,
--- src/navigation/DrawerNavigatorTab.js
+++ src/navigation/DrawerNavigatorTab.js
@@ -1,31 +1,44 @@
-import * as React from 'react';
+// import * as React from 'react';
+// import {View, Text} from 'react-native';
+// import {createDrawerNavigator} from '@react-navigation/drawer';
+// import RootClientTabs from './ClientTabs';
+// import {Icon} from 'react-native-elements';
+// import {colors} from '../global/styles';
+
+// const DrawerTab = createDrawerNavigator();
+
+// const DrawerNavigatorTab = () => {
+//   return (
+//     <DrawerTab.Navigator>
+//       <DrawerTab.Screen
+//         name="RootClientTabs"
+//         component={RootClientTabs}
+//         options={{
+//           title: 'Client',
+//           drawerIcon: ({focussed, size}) => {
+//             <Icon
+//               type="material-community"
+//               name="home"
+//               color={focussed ? colors.FOCUSSED : colors.grey2}
+//               size={size}
+//             />;
+//           },
+//         }}
+//       />
+//     </DrawerTab.Navigator>
+//   );
+// };
+
+// export default DrawerNavigatorTab;
+
+import React from 'react';
 import {View, Text} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import RootClientTabs from './ClientTabs';
-import {Icon} from 'react-native-elements';
-import {colors} from '../global/styles';
-
-const DrawerTab = createDrawerNavigator();
-
+import {} from 'react-native-gesture-handler';
 const DrawerNavigatorTab = () => {
   return (
-    <DrawerTab.Navigator>
-      <DrawerTab.Screen
-        name="RootClientTabs"
-        component={RootClientTabs}
-        options={{
-          title: 'Client',
-          drawerIcon: ({focussed, size}) => {
-            <Icon
-              type="material-community"
-              name="home"
-              color={focussed ? colors.FOCUSSED : colors.grey2}
-              size={size}
-            />;
-          },
-        }}
-      />
-    </DrawerTab.Navigator>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>DrawerNavigatorTab</Text>
+    </View>
   );
 };
 
